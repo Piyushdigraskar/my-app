b9c923b9e6dbfd0107c8a36075548a0f1eaefa07
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-01-25 17:17:35 +0530

    Adding expense manually

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItems(){
+    return (
+        <div>
+            <h2>Expense Item!</h2>
+            <h2>Food rs 10</h2>
+            <h2>Petrol rs 100</h2>
+            <h2>Movie rs 200</h2>
+        </div>
+    );
+}
+
+export default ExpenseItems;
