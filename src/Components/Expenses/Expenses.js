cf793a2525c5ec43d2ac54aa4e1f1580d109f649
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-02-02 09:46:38 +0530

    Adding yearWise Filter for Items

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -5,16 +5,19 @@ import ExpensesFilter from './ExpenseFilter';
 import { useState } from 'react';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredExpense, SetFilteredExpense] = useState(props.items);
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
+    const filteredExpense = props.items.filter((expense)=>expense.date.getFullYear().toString() === selectedYear);
+    SetFilteredExpense(filteredExpense);
   };
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
+        {filteredExpense.map((expense) => (
           <ExpenseItems
             key={expense.id}
             title={expense.title}
